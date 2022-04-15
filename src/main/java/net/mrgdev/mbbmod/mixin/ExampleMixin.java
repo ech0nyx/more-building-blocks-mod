29e1af67382c424bbfdc57ada381518de64f6891
Muhammadrizo <yusufovmuhammadrizogamedev@gmail.com>
2022-04-15 23:07:39 +0500

    Initial commit

---
+++ src/main/java/net/mrgdev/mbbmod/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.mrgdev.mbbmod.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.mrgdev.mbbmod.MoreBuildingBlocksMod;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		MoreBuildingBlocksMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
